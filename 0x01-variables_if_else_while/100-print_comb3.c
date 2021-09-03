eaafb305acce8077ee6edcd4c614f89305f9d62d
niibadger <bernardbadger1@gmail.com>
2021-09-03 14:29:02 -0700

    OKAY

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
 /**
- *main - print 
- *
- *Return: Always 0 (Success)
- */
+*main - print 
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-  int ones;
-  int tens;
+int ones;
+int tens;
 
-  for (tens = '0'; tens <= '9'; tens++)
-    {
-      for (ones = (tens + 1); ones <= '9'; ones++)
-	 {
-	   putchar(tens);
-	   putchar(ones);
+for (tens = '0'; tens <= '9'; tens++)
+{
+for (ones = (tens + 1); ones <= '9'; ones++)
+{
+putchar(tens);
+putchar(ones);
 
-	   if (tens != '8' || ones != '9')
-	     {
-	       putchar(',');
-	       putchar(' ');
-	     }
-	 }
-    }
-  putchar('\n');
-  return (0);
+if (tens != '8' || ones != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }

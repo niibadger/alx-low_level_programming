1da20ecdbe1285a571b937d5573bc9d4592a38e4
niibadger <bernardbadger1@gmail.com>
2021-09-02 17:33:59 -0700

    OUT AND OUT

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-sss
+#include <stdio.h>
+
+/**
+*main - print lowercase a-z
+*Return:Always 0 (Success)
+*/
+
+int main(void)
+{
+char alpha = "a" ;
+while (alpha <= "z")
+{
+putchar(alpha);
+alpha++;
+}
+putchar("\n");
+return (0);
+}

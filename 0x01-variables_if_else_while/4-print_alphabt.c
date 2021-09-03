7fea2590e32fd7ea56e36f2fcfc8341935903b2d
niibadger <bernardbadger1@gmail.com>
2021-09-02 18:36:56 -0700

    ORIGIN

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-sss
+#include <stdio.h>
+
+/**
+*main - print lowercase alpha a-z but remove 'q' and 'e'
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+if ((alpha != 'q') && (alpha != 'e'))
+{
+putchar(alpha);
+}
+alpha++;
+}
+putchar('\n');
+return (0);
+}

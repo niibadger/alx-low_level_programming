9cdce6f24cd6d4aef63f8683d009aeec935d9c8d
niibadger <bernardbadger1@gmail.com>
2021-09-02 19:08:16 -0700

    ORIGIN

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-ffff
+#include <stdio.h>
+
+/**
+*main - print alphabet reversed using putchar
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char alpha = 'z';
+while (alpha >= 'a')
+{
+putchar(alpha);
+alpha--;
+}
+putchar('\n');
+return (0);
+}

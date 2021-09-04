38479bc2fd1b2216796d10a241fa27e845020530
niibadger <bernardbadger1@gmail.com>
2021-09-04 11:51:58 -0700

    WE GOING HARD

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - Checks if a character is lowercase.
+ *@c: The character to be checked.
+ *
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
